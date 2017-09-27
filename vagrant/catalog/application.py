b41242cba5a95ed2a0a377a8cd945d8bcbc5c49a
Samuel Almeida <samuel.ralmeida@gmail.com>
2017-09-27 09:38:09 -0300

    feat: application skeleton
    create Flask app and app routes

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,2 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, url_for, flash
+
+app = Flask(__name__)
+
+@app.route('/login')
+def showLogin():
+    return "show login page"
+
+@app.route('/catalog.json')
+def JSONcatalog():
+    return "JSON de todo o catálogo"
+
+@app.route('/')
+@app.route('/catalog')
+def showCategories():
+    return "show all categories of recipes"
+
+@app.route('/catalog/<string:category_name>/items')
+def showItems(category_name):
+    return "show all items of category"
+
+@app.route('/catalog/<string:category_name>/<string:item_name>')
+def showItem(category_name, item_name):
+    return "show item was chose"
+
+@app.route('/catalog/<string:item_name>/edit')
+def editItem(item_name):
+    return "edit item was chose"
+
+@app.route('/catalog/<string:item_name>/delete')
+def deleteItem(item_name):
+    return "delete item was chose"
+
+if __name__ == '__main__':
+    app.secret_key = '^4u!gn!3Y8Fv'
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
