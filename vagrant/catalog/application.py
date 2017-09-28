db845172c1cae2e08eae261e4be81acd4e76988b
Samuel Almeida <samuel.ralmeida@gmail.com>
2017-09-28 17:00:17 -0300

    feat: create home page showing categories
    body: implemented functions to create category and list them, and home
    page draft

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, url_for, flash
+from flask import Flask, render_template, url_for, flash, request, redirect
+import crud
 
 app = Flask(__name__)
 
@@ -16,7 +17,22 @@ def JSONcatalog():
 @app.route('/')
 @app.route('/catalog')
 def showCategories():
-    return "show all categories of recipes"
+    categories = crud.findAllCategory()
+    return render_template('catalog.html', categories=categories)
+
+#this route only can be access for adim at the end of the app
+@app.route('/catalog/category/create', methods=['GET', 'POST'])
+def createCategory():
+    if request.method == 'POST':
+        new_category_name = request.form['name']
+        crud_function = crud.newCategory(new_category_name)
+        if crud_function:
+            return render_template('newcategory.html', error=crud_function)
+        else:
+            flash('Category has been created')
+            return redirect(url_for('showCategories'))
+    else:
+        return render_template('newcategory.html')
 
 @app.route('/catalog/<string:category_name>/items')
 def showItems(category_name):
--- vagrant/catalog/crud.py
+++ vagrant/catalog/crud.py
@@ -1,2 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, User, Category, Item, engine
+
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+def findAllCategory():
+    return session.query(Category).all()
+
+def findCategory(name):
+    return session.query(Category).filter_by(name=name).first()
+
+def newCategory(name):
+    result = findCategory(name)
+    if result:
+        error = "A category with that name already exists"
+        return error
+    else:
+        new_category = Category(name=name)
+        session.add(new_category)
+        session.commit()
+        return None
